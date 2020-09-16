284d25653724d47a33fe7938602aa623af81d4c4
Brendan Luther <brendanluther15@gmail.com>
2020-09-15 21:57:30 -0400

    Integrating Image Files

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #Starting from scratch using Git in VSCODE
 from flask import Flask, render_template, request, url_for
-import weather_scraper
+import weatherScraper
 
 
 app = Flask(__name__)
@@ -13,31 +13,28 @@ lon_piank = '-76.3'
 lat_york = '37.3'
 lon_york = '-76.4'
 
+
 @app.route('/')
 def hello_world():
     return render_template('home.html')
-    /Users/brendanluther/Desktop/Py_Projects/TideWeatherWebsite/TidewaterWebsite/app.py
 
 @app.route('/james')
 def james():
     weath = weather_scraper.weath(lat_james, lon_james)
     wind = int(float(weather_scraper.wind(lat_james, lon_james)) * 2.23) #converting m/s to mph
-    wdir = weather_scraper.wind_dir(lat_james, lon_james)
+    wdir = weatherScraper.wind_dir(lat_james, lon_james)
     return render_template('james.html', weather=weath, wind=wind, windDir=wdir)
 
 @app.route('/piankatank')
 def piank():
     weath = weather_scraper.weath(lat_piank, lon_piank)
     wind = int(float(weather_scraper.wind(lat_piank, lon_piank)) * 2.23) #converting m/s to mph
-    wdir = weather_scraper.wind_dir(lat_piank, lon_piank)
+    wdir = weatherScraper.wind_dir(lat_piank, lon_piank)
     return render_template('piankatank.html', weather=weath, wind=wind, windDir=wdir)
 
 @app.route('/york')
 def york():
-    weath = weather_scraper.weath(lat_york, lon_york)
-    wind = int(float(weather_scraper.wind(lat_york, lon_york)) * 2.23) #converting m/s to mph
-    wdir = weather_scraper.wind_dir(lat_york, lon_york)
-    return render_template('york.html', weather=weath, wind=wind, windDir=wdir)
+    return render_template('york.html')
 
 
 
