6968f27cd3e8eeb6eb4c1ae713781382c0ea2c15
Brendan Luther <brendanluther15@gmail.com>
2020-10-18 13:57:30 -0400

    Changed for testing

--- stampMaker.py
+++ stampMaker.py
@@ -4,7 +4,7 @@ import datetime
 
 tidal_james_delta = 360
 piankatank_delta = 21480
-mobjack_delta = 28800
+york_delta = 28800
 
 
 def stamp_maker(time_since_last_low):
@@ -17,4 +17,4 @@ def stamp_maker(time_since_last_low):
 if __name__ == ('__main__'):
     print(stamp_maker(tidal_james_delta))
     print(stamp_maker(piankatank_delta))
-    print(stamp_maker(mobjack_delta))
+    print(stamp_maker(york_delta))
